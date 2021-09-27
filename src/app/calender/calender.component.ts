43c737f3f12804589c806bd86577d49dc602a0bb
Sivakumar1103 <rajesh.r@aikyne.com>
2021-09-27 18:56:11 +0530

    27/09

--- src/app/calender/calender.component.ts
+++ src/app/calender/calender.component.ts
@@ -1,48 +1,50 @@
-import { Component,   ChangeDetectionStrategy,
+import {
+  Component, ChangeDetectionStrategy,
   ViewChild,
-  TemplateRef, } from '@angular/core';
+  TemplateRef,
+} from '@angular/core';
 
-  import {
-    startOfDay,
-    endOfDay,
-    subDays,
-    addDays,
-    endOfMonth,
-    isSameDay,
-    isSameMonth,
-    addHours,
-  } from 'date-fns';
-  import { Subject } from 'rxjs';
-  import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
-  import {
-    CalendarEvent,
-    CalendarEventAction,
-    CalendarEventTimesChangedEvent,
-    CalendarView,
-  } from 'angular-calendar';
-  
-  const colors: any = {
-    red: {
-      primary: '#ad2121',
-      secondary: '#FAE3E3',
-    },
-    blue: {
-      primary: '#1e90ff',
-      secondary: '#D1E8FF',
-    },
-    yellow: {
-      primary: '#e3bc08',
-      secondary: '#FDF1BA',
-    },
-  };
-  
+import {
+  startOfDay,
+  endOfDay,
+  subDays,
+  addDays,
+  endOfMonth,
+  isSameDay,
+  isSameMonth,
+  addHours,
+} from 'date-fns';
+import { Subject } from 'rxjs';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import {
+  CalendarEvent,
+  CalendarEventAction,
+  CalendarEventTimesChangedEvent,
+  CalendarView,
+} from 'angular-calendar';
+
+const colors: any = {
+  red: {
+    primary: '#ad2121',
+    secondary: '#FAE3E3',
+  },
+  blue: {
+    primary: '#1e90ff',
+    secondary: '#D1E8FF',
+  },
+  yellow: {
+    primary: '#e3bc08',
+    secondary: '#FDF1BA',
+  },
+};
+
 @Component({
   selector: 'app-calender',
   templateUrl: './calender.component.html',
   styleUrls: ['./calender.component.css']
 })
-export class CalenderComponent  {
-   @ViewChild('modalContent', { static: true })
+export class CalenderComponent {
+  @ViewChild('modalContent', { static: true })
   modalContent!: TemplateRef<any>;
 
   view: CalendarView = CalendarView.Month;
@@ -56,32 +58,32 @@ export class CalenderComponent  {
   };
 
   actions: CalendarEventAction[] = [
-   
-   
-   
-   
-   
-   
-   
+
+
+
+
+
+
+
     {
       label: '',
       a11yLabel: '',
       onClick: ({ event }: { event: CalendarEvent }): void => {
         this.events = this.events.filter((iEvent) => iEvent !== event);
-        
+
       },
     },
   ];
 
   refresh: Subject<any> = new Subject();
 
-  
+
   events: CalendarEvent[] = [
     // 
     {
       start: subDays(startOfDay(new Date()), 1),
       end: addDays(new Date(), 0),
-      title: 'sfdgsd',
+      title: 'ssdgfhghtgfhtygj',
       color: colors.red,
       actions: this.actions,
       allDay: true,
@@ -93,29 +95,29 @@ export class CalenderComponent  {
     },
 
     // {
-      // start: startOfDay(new Date()),
-      // title: 'An event with no end date',
-      // color: colors.yellow,
-      // actions: this.actions,
+    // start: startOfDay(new Date()),
+    // title: 'An event with no end date',
+    // color: colors.yellow,
+    // actions: this.actions,
     // },
     // {
-      // start: subDays(endOfMonth(new Date()), 3),
-      // end: addDays(endOfMonth(new Date()), 3),
-      // title: 'A long event that spans 2 months',
-      // color: colors.blue,
-      // allDay: true,
+    // start: subDays(endOfMonth(new Date()), 3),
+    // end: addDays(endOfMonth(new Date()), 3),
+    // title: 'A long event that spans 2 months',
+    // color: colors.blue,
+    // allDay: true,
     // },
     // {
-      // start: addHours(startOfDay(new Date()), 2),
-      // end: addHours(new Date(), 2),
-      // title: 'A draggable and resizable event',
-      // color: colors.yellow,
-      // actions: this.actions,
-      // resizable: {
-        // beforeStart: true,
-        // afterEnd: true,
-      // },
-      // draggable: true,
+    // start: addHours(startOfDay(new Date()), 2),
+    // end: addHours(new Date(), 2),
+    // title: 'A draggable and resizable event',
+    // color: colors.yellow,
+    // actions: this.actions,
+    // resizable: {
+    // beforeStart: true,
+    // afterEnd: true,
+    // },
+    // draggable: true,
     // },
   ];
 
@@ -188,6 +190,6 @@ export class CalenderComponent  {
   closeOpenMonthViewDay() {
     this.activeDayIsOpen = false;
   }
- 
+
 
 }
